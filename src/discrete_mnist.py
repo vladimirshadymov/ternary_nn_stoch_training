89b2259359eaa29ffd6a0bd557126641ac0f1d60
vladimirshadymov <vladimir.shadymov@gmail.com>
2021-02-11 11:17:34 +0300

    Add training files

---
+++ src/discrete_mnist.py
@@ -0,0 +1,123 @@
+from __future__ import print_function
+import os
+import csv
+from itertools import zip_longest
+import torch.nn as nn
+from layers import DiscretizedLinear, Discretization
+from training_routines import train, test
+import argparse
+import torch
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import MultiStepLR, CosineAnnealingLR, ReduceLROnPlateau
+import numpy as np
+
+
+class MnistDense3NN(nn.Module):
+    def __init__(self, hidden_layer_size=100, discrt_lvls=3):
+        super(MnistDenseBNN, self).__init__()
+        self.hidden_layer_size = hidden_layer_size
+        self.min_weight = -1.0
+        self.max_weight = +1.0
+
+        self.layer1 = nn.Sequential(
+            DiscretizedLinear(in_features=28*28, out_features=hidden_layer_size, min_weight=self.min_weight, max_weight=self.max_weight)
+            nn.LayerNorm(hidden_layer_size),
+            nn.Tanh()
+        )
+
+        self.layer2 = nn.Sequential(
+            DiscretizedLinear(in_features=hidden_layer_size, out_features=hidden_layer_size, min_weight=self.min_weight, max_weight=self.max_weight)
+            nn.LayerNorm(hidden_layer_size),
+            nn.Tanh()
+        )
+
+        self.layer3 = nn.Sequential(
+            DiscretizedLinear(in_features=hidden_layer_size, out_features=10, min_weight=self.min_weight, max_weight=self.max_weight)
+            nn.LayerNorm(10)
+        )
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        return x
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--cuda-num', type=int, default=0,
+                        help='Choses GPU number')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--lower-output', type=float, default=-1, metavar='LR',
+                        help='lower output (default: -1)')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    
+    HIDDEN_SIZE = 150
+    DISCRETE_LVLS_NUMBER = 2
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda:%d" % args.cuda_num if torch.cuda.is_available() and use_cuda else "cpu")
+    print("Use device:", device)
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+    model = MnistDense3NN(HIDDEN_SIZE, DISCRETE_LVLS_NUMBER).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-10)
+    scheduler = MultiStepLR(optimizer, milestones=[20,80,150,250,400], gamma=0.7)
+
+    test_accuracy = []
+    train_accuracy = []
+
+    for epoch in range(1, args.epochs + 1):
+        print('Epoch:', epoch, 'LR:', scheduler.get_lr())
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader, train_loader, test_accuracy, train_accuracy) 
+        scheduler.step()
+        
+        if epoch>=10:
+            if (args.save_model):
+                torch.save(model.state_dict(), f"../model/mnist_3nn_{HIDDEN_SIZE}_discrt_lvls_{DISCRETE_LVLS_NUMBER}.pt")
+            d = [train_accuracy, test_accuracy]
+            export_data = zip_longest(*d, fillvalue='')
+            with open(f'../model/mnist_3nn_report_{HIDDEN_SIZE}_discrt_lvls_{DISCRETE_LVLS_NUMBER}.csv', 'w', encoding="ISO-8859-1", newline='') as report_file:
+                wr = csv.writer(report_file)
+                wr.writerow(("Train accuracy", "Test accuracy"))
+                wr.writerows(export_data)
+            report_file.close()
+
+if __name__ == '__main__':
+    main()
---
+++ src/training_routines.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from binarized_layers import BinarizedLinear
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        # target_new = target.reshape(1, 1)
+        # num_classes = 10
+        # one_hot_target = (target_new == torch.arange(num_classes).reshape(1, num_classes).to(device)).float().add(-0.1).div(0.3)
+        # loss = F.mse_loss(output, one_hot_target, reduction='sum')
+        loss = F.cross_entropy(output, target, reduction='mean')
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+def test(args, model, device, test_loader, train_loader=None, test_accuracy=None, train_accuracy=None):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            # target_new = target.reshape(1, 1)
+            # num_classes = 10
+            # one_hot_target = (target_new == torch.arange(num_classes).reshape(1, num_classes).to(device)).float().add(-0.1).div(0.3)
+            # test_loss += F.mse_loss(output, one_hot_target, reduction='sum').item()
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    if test_accuracy is not None:
+        test_accuracy.append(100. * correct / len(test_loader.dataset))
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    if not train_loader is None:
+        correct = 0
+        train_loss = 0
+        with torch.no_grad():
+            for data, target in train_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                target_new = target.reshape(1, 1)
+                num_classes = 10
+                one_hot_target = (target_new == torch.arange(num_classes).reshape(1, num_classes).to(device)).float().add(-0.1).div(0.3)
+                test_loss += F.mse_loss(output, one_hot_target, reduction='sum').item()
+                # train_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+                pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        train_loss /= len(train_loader.dataset)
+
+        if train_accuracy is not None:
+            train_accuracy.append(100. * correct / len(train_loader.dataset))
+
+        print('Train set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            train_loss, correct, len(train_loader.dataset),
+            100. * correct / len(train_loader.dataset)))
