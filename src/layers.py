a86826f70be8b6bd48d11bba23792d07054f0272
vladimirshadymov <vladimir.shadymov@gmail.com>
2021-02-11 10:36:00 +0300

    Add N dicrete levels func-module class

---
+++ src/layers.py
@@ -0,0 +1,27 @@
+# Import PyTorch
+import torch  # import main library
+from torch.autograd import Function  # import Function to create custom activations
+from torch import nn
+from torch.nn.parameter import Parameter
+from torch.nn import functional as F
+import math
+import numpy as np
+
+class DiscreteHardtanhFunction(Function):
+
+    @staticmethod
+    def forward(ctx, input, discrt_lvls=2):
+        ctx.save_for_backward(input)  # save input for backward pass
+
+        return F.hardtanh(input).add(1.).mul(discrt_lvls/2).round().div(discrt_lvls/2).add(-1.)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+
+        grad_input = input.clone()
+        grad_input[torch.abs(input) <= 1 ] = 1.
+        grad_input[torch.abs(input) > 1 ] = 0.
+        grad_input = grad_input * grad_output
+
+        return grad_input
