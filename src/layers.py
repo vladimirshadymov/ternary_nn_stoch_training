4798d918d199e6150711d6f0445d8f77b92ab9dc
vladimirshadymov <vladimir.shadymov@gmail.com>
2021-02-11 10:59:21 +0300

    Add linear layer with discrete weights

--- src/layers.py
+++ src/layers.py
@@ -28,10 +28,23 @@ class DiscreteHardtanhFunction(Function):
 
 class Discretization(nn.Module):
     def __init__(self, min=-1, max=1, discrt_lvls=2):
-        super(Binarization, self).__init__()
+        super(Discretization, self).__init__()
         self.min = min
         self.max = max
         self.discrt_lvls = discrt_lvls
 
     def forward(self, input):
         return 0.5*(DiscreteHardtanhFunction.apply(input, self.discrt_lvls)*(self.max - self.min) + self.min + self.max)
+
+class DiscretizedLinear(nn.Linear):
+
+    def __init__(self, min_weight=-1, max_weight=1, discrt_lvls=2, *kargs, **kwargs):
+        super(DiscretizedLinear, self).__init__(*kargs, **kwargs)
+        self.discretization = Discretization(min=min_weight, max=max_weight, discrt_lvls=discrt_lvls)
+        self.min_weight = min_weight
+        self.max_weight = max_weight
+        self.discrt_lvls = discrt_lvls
+
+    def forward(self, input):
+        self.weight.data = F.hardtanh_(self.weight.data)
+        return F.linear(input, self.discretization(self.weight), bias=self.bias)
